6402c49fc2bd8e8960c76318ef1f1734d624cb54
Raptor <ouatis@gmail.com>
2016-10-30 13:06:36 +0100

    65. Unreal's Player Controller

--- Source/Escape/OpenDoor.cpp
+++ Source/Escape/OpenDoor.cpp
@@ -21,15 +21,31 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	//Get Owner
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+void UOpenDoor::OpenDoor()
+{
+	//Find the owning Actor
 	AActor* Owner = GetOwner();
 
 	//Create a rotator
-	FRotator NewRotation = FRotator(0.0f, 120.0f, 0.0f);
+	FRotator NewRotation = FRotator(0.0f, OpenAngle, 0.0f);
 
 	//Set the door rotation
 	Owner->SetActorRotation(NewRotation);
-	
+}
+
+void UOpenDoor::CloseDoor()
+{
+	//Find the owning Actor
+	AActor* Owner = GetOwner();
+
+	//Create a rotator
+	FRotator NewRotation = FRotator(0.0f, ClosedAngle, 0.0f);
+
+	//Set the door rotation
+	Owner->SetActorRotation(NewRotation);
 }
 
 
@@ -38,6 +54,13 @@ void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompo
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
-	// ...
+	//Poll the Trigger Volume every frame
+	//If the ActorThatOpens is in the volume
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor();
+	}
+	else
+		CloseDoor();
 }
 
--- Source/Escape/OpenDoor.h
+++ Source/Escape/OpenDoor.h
@@ -17,10 +17,27 @@ public:
 
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+	// Called when pressure plate opens door
+	virtual void OpenDoor();
+
+	// Called when door closes
+	virtual void CloseDoor();
 	
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
-		
+private:
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 120.0f;
+
+	UPROPERTY(VisibleAnywhere)
+	float ClosedAngle = 180.0f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+
+	UPROPERTY(VisibleAnywhere)
+	AActor* ActorThatOpens; //Pawn inherits from actor
 	
 };
